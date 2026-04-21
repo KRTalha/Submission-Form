34e08e3759cf5d5e4ee1ec2833a92b4d9f4a4e26
Talha Mahmood <rana.talha20003@gmail.com>
2026-04-21 15:19:15 +0200

    Added the first version withot css

---
+++ jokegenerator/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Joke from "./Joke";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Joke Generator Using React and Joke API</h1>
+      <Joke />
+    </div>
+  );
+}
+
+export default App;
---
+++ jokegenerator/src/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Button.css";
+
+const Button = (props) => {
+  return <button onClick={props.callApi}>Click to Generate a Joke.</button>;
+};
+export default Button;
